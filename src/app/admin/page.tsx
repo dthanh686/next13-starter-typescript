5de10a10edd0f2e8393fe5b9e7bbeb0e44c10096
thanhtd <thanhtd@vnext.vn>
2023-09-18 23:24:39 +0700

    update

--- src/app/admin/page.tsx
+++ src/app/admin/page.tsx
@@ -1,11 +1,14 @@
 "use client";
 import React from "react";
 import { useRouter } from "next/navigation";
+import { Button } from "react-bootstrap";
+import "bootstrap/dist/css/bootstrap.min.css";
 
 const Page = () => {
   const router = useRouter();
   return (
     <>
+      <Button variant="primary">Button</Button>
       <div>Page</div>
       <button onClick={() => router.push("/")}>Back to Home</button>
     </>
