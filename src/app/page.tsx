2e21ec5b73bd84702d54932ef5270f73171adc9f
thanhtd <thanhtd@vnext.vn>
2023-09-18 23:15:13 +0700

    update scss

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,10 +1,14 @@
 import Link from "next/link";
+import t from "@/styles/app.module.css";
+import d from "@/styles/thanhtd.module.css";
 
 const Home = () => {
   return (
     <>
-      <div>
-        <a href="/admin">Admin</a>
+      <div className={t["red"]}>
+        <a className={d["red"]} href="/admin">
+          Admin
+        </a>
       </div>
       <Link href="/admin">Test Link</Link>
     </>
